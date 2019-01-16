29e4b2f71d12744d2eb1c8480d7151abae23e1e5
ykojim <ykojim@gmail.com>
2019-01-16 23:32:23 +0900

    add localization

--- mod001/src/main/java/stsmodstudy/Flare.java
+++ mod001/src/main/java/stsmodstudy/Flare.java
@@ -4,58 +4,60 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
-
 import basemod.abstracts.CustomCard;
 
+// カードはCustomCardクラスを継承して定義する
 public class Flare extends CustomCard {
-    public static final String ID = "myModID:Flare";
-    // TODO:
-    // private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    // Get object containing the strings that are displayed in the game.
-    // public static final String NAME = cardStrings.NAME;
-    public static final String NAME = "hello_my_card";
-    // public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String DESCRIPTION = "hello_my_description";
+    public static final String ID = "stsmodstudy:Flare";
+    // getCardStringsで Mainクラスにて読み込んだ cards-JPN.json 内の文字列情報を取得する
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG_PATH = "assets/img/relics/BursterCore.png";
     private static final int COST = 0;
-    private static final int ATTACK_DMG = 3;
+    private static final int ATTACK_DMG = 4;
     private static final int UPGRADE_PLUS_DMG = 3;
     private static final int VULNERABLE_AMT = 1;
     private static final int UPGRADE_PLUS_VULNERABLE = 1;
 
     public Flare() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
-        		AbstractCard.CardType.ATTACK, AbstractCard.CardColor.RED,
-        		AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
+                AbstractCard.CardType.ATTACK,
+                AbstractCard.CardColor.RED,
+                AbstractCard.CardRarity.COMMON,
+                AbstractCard.CardTarget.ENEMY);
         this.magicNumber = this.baseMagicNumber = VULNERABLE_AMT;
         this.damage=this.baseDamage = ATTACK_DMG;
         
         this.setBackgroundTexture("assets/img/relics/BursterCore.png", "assets/img/relics/BursterCore.png");
-
-        this.setOrbTexture("assets/img/relics/BursterCore.png", "assets/img/relics/BursterCore.png");
-
         this.setBannerTexture("assets/img/relics/BursterCore.png", "assets/img/relics/BursterCore.png");
     }
 
+    // カード使用時に発動する効果
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-    	AbstractDungeon.actionManager.addToBottom(
-    		new com.megacrit.cardcrawl.actions.common.DamageAction(
-    			m,
-				new DamageInfo(p, this.damage, this.damageTypeForTurn),
-				AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-    	AbstractDungeon.actionManager.addToBottom(
-    		new ApplyPowerAction(
-    			m,
-    			p,
-    			new VulnerablePower(m, this.magicNumber, false),
-    			this.magicNumber,
-    			true,
-    			AbstractGameAction.AttackEffect.NONE));
+        // ダメージを与える
+        AbstractDungeon.actionManager.addToBottom(
+            new com.megacrit.cardcrawl.actions.common.DamageAction(
+                m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn),
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL) // 画面効果
+        );
+        // 脆弱化をかける
+        AbstractDungeon.actionManager.addToBottom(
+            new ApplyPowerAction(                                // バフ/デバフはすべて内部的にはパワー扱い
+                m,
+                p,
+                new VulnerablePower(m, this.magicNumber, false), // 脆弱をm(=敵)にかける
+                this.magicNumber,
+                true,
+                AbstractGameAction.AttackEffect.NONE)            // 画面効果
+        );
     }
 
     @Override
@@ -63,6 +65,7 @@ public class Flare extends CustomCard {
         return new Flare();
     }
 
+    // カードアップグレード時の処理
     @Override
     public void upgrade() {
         if (!this.upgraded) {
--- mod001/src/main/java/stsmodstudy/Main.java
+++ mod001/src/main/java/stsmodstudy/Main.java
@@ -1,21 +1,35 @@
 import basemod.BaseMod;
 import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.CardStrings;
 
 @SpireInitializer
-public class Main implements EditCardsSubscriber {
+public class Main implements
+  EditCardsSubscriber,   // カードを追加する場合にimplementする
+  EditStringsSubscriber  // 言語ファイルを読み込む場合に implementする
+{
 
     public Main(){
         BaseMod.subscribe(this);
     }
 
+    // @SpireInitializerで修飾したクラスはこのメソッドを定義する必要がある
     public static void initialize() {
-        // logger.info("Initializing Mod");
         Main main = new Main();
     }
 
     @Override
+    public void receiveEditStrings() {
+        // 独自定義した言語ファイルを追加
+        // Settings.languageには日本語ならJPN, 英語ならENGが入っている
+        BaseMod.loadCustomStringsFile(CardStrings.class, "assets/loc/cards-" + Settings.language + ".json");
+    }
+
+    @Override
     public void receiveEditCards() {
+        // 独自定義したカードを追加
         BaseMod.addCard(new Flare());
     }
 }
